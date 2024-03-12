8aa35e6aa65f6b266f4e160c57887ed8178533cd
AlexandreBottet <alexandre.bottet@outlook.fr>
2024-03-12 15:30:49 +0100

    feat: Implement a function drawBoard to create board

--- js/app.js
+++ js/app.js
@@ -7,5 +7,28 @@ const app = {
     targetCell: {
         x: 5,
         y: 3
+    },
+    drawBoard: function() {
+        const numOfRows = 4;
+        const numOfCols = 6;
+
+        const board = document.getElementById('board');
+
+        for (let i = 0; i < numOfRows; i++) {
+            const row = document.createElement('div');
+            row.classList.add('row');
+
+            for (let j = 0; j < numOfCols; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+        
+                row.appendChild(cell);
+            }
+
+            board.appendChild(row);
+        }
+
     }
 }
+
+app.drawBoard();
